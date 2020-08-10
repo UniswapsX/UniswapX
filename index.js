90223109e68fe9a517d644e6395fd2e571f3e491
UniswapsX <UniswapsX@gmail.com>
2020-08-11 00:38:05 +0800

    修改样式

--- index.js
+++ index.js
@@ -1,109 +1,122 @@
 var thisIndex = 0; //所处页面
 
-new fullpage('#fullpage', {
-    anchors: ['interval', 'homepage', 'mensaprotocol', 'mensaearn', 'token', 'partner', 'joinus'],
-    // sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
-    responsiveHeight: 410,
-    // navigation: true,
-    // navigationTooltips: ['fullPage', 'Open', 'Easy', 'Touch'],
-    afterLoad: function (origin, destination, direction) {
-        // console.log('回调页面',origin)
-        thisIndex = origin.index;
-        changeState();
-    },
-
+new fullpage("#fullpage", {
+  anchors: [
+    "interval",
+    "homepage",
+    "mensaprotocol",
+    "mensaearn",
+    "token",
+    "partner",
+    "joinus",
+  ],
+  // sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+  responsiveHeight: 410,
+  // navigation: true,
+  // navigationTooltips: ['fullPage', 'Open', 'Easy', 'Touch'],
+  afterLoad: function (origin, destination, direction) {
+    // console.log('回调页面',origin)
+    thisIndex = origin.index;
+    changeState();
+  },
 });
 var daojishi = null;
 var padTime = null;
-var middle = document.querySelector('#productfather');
-var middleSecond = document.querySelector('#padfather');
-var closeIcon = document.querySelector('.arrow-down');
-var openIcon = document.querySelector('.arrow-up')
-middle.addEventListener('mouseover', hoverEvent);
-middle.addEventListener('onclick', hoverEvent);
-middle.addEventListener('mouseout', hoverEventTwo);
-middleSecond.addEventListener('mouseover', hoverEvents);
-middleSecond.addEventListener('mouseout', levelEvent);
+var middle = document.querySelector("#productfather");
+var middleSecond = document.querySelector("#padfather");
+var closeIcon = document.querySelector(".arrow-down");
+var openIcon = document.querySelector(".arrow-up");
+middle.addEventListener("mouseover", hoverEvent);
+middle.addEventListener("onclick", hoverEvent);
+middle.addEventListener("mouseout", hoverEventTwo);
+middleSecond.addEventListener("mouseover", hoverEvents);
+middleSecond.addEventListener("mouseout", levelEvent);
 
 function hoverEvent() {
-    if (daojishi) {
-        clearTimeout(daojishi);
-        daojishi = null;
-    }
-    document.querySelector('.list-menu').style.display = 'block';
+  if (daojishi) {
+    clearTimeout(daojishi);
+    daojishi = null;
+  }
+  document.querySelector(".list-menu").style.display = "block";
 }
 
 function hoverEventTwo() {
-    daojishi = setTimeout(() => {
-        document.querySelector('.list-menu').style.display = 'none';
-    }, 400);
+  daojishi = setTimeout(() => {
+    document.querySelector(".list-menu").style.display = "none";
+  }, 400);
 }
 
 function hoverEvents() {
-    if (padTime) {
-        clearTimeout(padTime);
-        padTime = null;
-    }
-    document.querySelector('.list-menu-second').style.display = 'block';
+  if (padTime) {
+    clearTimeout(padTime);
+    padTime = null;
+  }
+  document.querySelector(".list-menu-second").style.display = "block";
 }
 
 function levelEvent() {
-    padTime = setTimeout(() => {
-        document.querySelector('.list-menu-second').style.display = 'none';
-    }, 400);
+  padTime = setTimeout(() => {
+    document.querySelector(".list-menu-second").style.display = "none";
+  }, 400);
 }
 
 function goPage() {
-    fullpage_api.moveSectionDown();
+  fullpage_api.moveSectionDown();
 }
 
 function changeState() {
-    if (fullpage_api.getActiveSection().index < 7) {
-        document.querySelector('.footer').style.display = 'block';
-    } else {
-        document.querySelector('.footer').style.display = 'none';
-    }
+  if (fullpage_api.getActiveSection().index < 7) {
+    document.querySelector(".footer").style.display = "block";
+  } else {
+    document.querySelector(".footer").style.display = "none";
+  }
 }
 
 function toDate(time) {
-    // 计算天数
-    const days = Math.floor(time / (24 * 3600 * 1000)).toString().padStart(2, '0')
+  // 计算天数
+  const days = Math.floor(time / (24 * 3600 * 1000))
+    .toString()
+    .padStart(2, "0");
 
-    // 计算出小时数
-    const leave1 = time % (24 * 3600 * 1000) // 计算天数后剩余的毫秒数
-    const hours = Math.floor(leave1 / (3600 * 1000)).toString().padStart(2, '0')
-    // 计算相差分钟数
-    const leave2 = leave1 % (3600 * 1000) // 计算小时数后剩余的毫秒数
-    const minutes = Math.floor(leave2 / (60 * 1000)).toString().padStart(2, '0')
-    // 计算相差秒数
-    const leave3 = leave2 % (60 * 1000) // 计算分钟数后剩余的毫秒数
-    const seconds = Math.round(leave3 / 1000).toString().padStart(2, '0')
-    const ret = {
-        days,
-        hours,
-        minutes,
-        seconds
-    }
-    Object.keys(ret).forEach(key => {
-        if (ret[key] < 0) ret[key] = '00'
-    })
-    return ret
+  // 计算出小时数
+  const leave1 = time % (24 * 3600 * 1000); // 计算天数后剩余的毫秒数
+  const hours = Math.floor(leave1 / (3600 * 1000))
+    .toString()
+    .padStart(2, "0");
+  // 计算相差分钟数
+  const leave2 = leave1 % (3600 * 1000); // 计算小时数后剩余的毫秒数
+  const minutes = Math.floor(leave2 / (60 * 1000))
+    .toString()
+    .padStart(2, "0");
+  // 计算相差秒数
+  const leave3 = leave2 % (60 * 1000); // 计算分钟数后剩余的毫秒数
+  const seconds = Math.round(leave3 / 1000)
+    .toString()
+    .padStart(2, "0");
+  const ret = {
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
+  Object.keys(ret).forEach((key) => {
+    if (ret[key] < 0) ret[key] = "00";
+  });
+  return ret;
 }
-let targetDate = new Date(1597320000000)
+let targetDate = new Date(1597320000000);
 
 function updateContent() {
-    let {
-        days,
-        hours,
-        minutes,
-        seconds
-    } = toDate(targetDate - Date.now())
-    let time = days + hours + minutes + seconds
-    let intervalItems = document.querySelectorAll('.interval-group-item')
-    Array.from(intervalItems).forEach((item, index) => {
-        item.innerText = time[index]
-    })
+  let { days, hours, minutes, seconds } = toDate(targetDate - Date.now());
+  let time = days + hours + minutes + seconds;
+  let intervalItems = document.querySelectorAll(".interval-group-item");
+  Array.from(intervalItems).forEach((item, index) => {
+    item.innerText = time[index];
+  });
 }
-updateContent()
+updateContent();
 
-let countDownInterval = setInterval(updateContent, 1000)
+let countDownInterval = setInterval(updateContent, 1000);
+// window.addEventListener("resize", function () {
+//   console.log(window.pageXOffset, "屏幕宽度");
+// });
